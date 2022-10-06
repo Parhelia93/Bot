19166c417a6e8a4251d34730ab1e5f749e837a0e
Parhelia__ <ostapchukalekseyjdm@gmail.com>
2022-10-06 23:13:53 +0300

    Added utils func

--- src/bot_app/random_ten.py
+++ src/bot_app/random_ten.py
@@ -1,4 +1,3 @@
-
 from aiogram import types
 from bot_app.states import GameStates
 from .app import dp
@@ -7,73 +6,39 @@ from .data_fetcher import get_random, put_answer
 from aiogram.dispatcher import FSMContext
 from .messages import EMPTY_DICTIONARY_EMPTYING, TRAINING_DONE, WRONG_ANSWER
 from .local_settings import CNT_TRAINT_STEP
-from .utils import get_random_word_detail, get_all_translate
+from .utils import prepare_dict, find_answer
 
 @dp.message_handler(commands='train_ten', state="*")
 async def train_ten(message: types.Message, state: FSMContext):
     await GameStates.random_ten.set()
     res = await get_random(message.from_user.id)
     async with state.proxy() as data:
-        word = res.get('word')
-        word_detail = res.get('words_detail')
-        translate, example, stat_id = get_random_word_detail(word_detail)
-        translate_list = get_all_translate(word_detail)
-        
-        data['step'] = 1
-        data['word'] = word
-        data['translate'] = translate_list
-        data['example'] = example
-        data['stat_id'] = stat_id
-        data['telegram_id'] = str(message.from_user.id)
-        if not data['word']:
+        result = prepare_dict(res)
+        await message.answer(result)
+        data.update(step=1, word=result.get('word'), translate=result.get('translate_list'), example=result.get('example'), stat_id=result.get('stat_id'), telegram_id=str(message.from_user.id), all_pk=result.get('all_pk'), all_message=result.get('all_msg'))
+        if not res:
             await state.finish()
             return await message.reply(EMPTY_DICTIONARY_EMPTYING)
         await message.reply(f"Step: {data['step']}, {data['word']}, {data['stat_id']}")
 
 
-# @dp.message_handler(lambda message: message.text not in ["/start", "/help", "/asd"], state=GameStates.random_ten)
-# async def answer_handler(message: types.Message, state: FSMContext):
-#     async with state.proxy() as data:
-#         if message.text in data.get('translate'):
-#             user_answer_dict = {'pk': data.get('stat_id'), 'answer': 'True'}
-#             await put_answer(user_answer_dict)
-#             data['step']+=1
-#             if data['step'] > CNT_TRAINT_STEP:
-#                 await state.finish()
-#                 return await message.answer(TRAINING_DONE)    
-#             res = await get_random(message.from_user.id)
-#             data['translate'] = res.get('translate')
-#             data['word'] = res.get('word')
-#             data['example'] = res.get('example')
-#             data['stat_id'] = res.get('word_stat').get('pk')
-#             data['telegram_id'] = str(message.from_user.id)
-#             await message.answer(f"Step: {data['step']}, {data['word']}, {data['stat_id']}")
-#         else:
-#             await message.answer(WRONG_ANSWER)
-
 @dp.message_handler(lambda message: message.text not in ["/start", "/help", "/asd"], state=GameStates.random_ten)
 async def answer_handler(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         if message.text in data.get('translate'):
-            user_answer_dict = {'pk': data.get('stat_id'), 'answer': 'True'}
-            await put_answer(user_answer_dict)
             data['step']+=1
+            true_pk = find_answer(data.get('all_message'), message.text)
+            await put_answer({'pk': true_pk, 'answer': 'True'})
+            
             if data['step'] > CNT_TRAINT_STEP:
                 await state.finish()
-                return await message.answer(TRAINING_DONE)    
+                return await message.answer(TRAINING_DONE)
+
             res = await get_random(message.from_user.id)
-            word = res.get('word')
-            word_detail = res.get('words_detail')
-            translate, example, stat_id = get_random_word_detail(word_detail)
-            translate_list = get_all_translate(word_detail)
-            data['translate'] = translate
-            data['word'] = word
-            data['example'] = example
-            data['stat_id'] = stat_id
-            data['telegram_id'] = str(message.from_user.id)
+            result = prepare_dict(res)
+            data.update(word=result.get('word'), translate=result.get('translate_list'), example=result.get('example'), stat_id=result.get('stat_id'), telegram_id=str(message.from_user.id), all_pk=result.get('all_pk'), all_message=result.get('all_msg'))
             await message.answer(f"Step: {data['step']}, {data['word']}, {data['stat_id']}")
         else:
             await message.answer(WRONG_ANSWER)
-
-
-            # Добавить функцию, увеличиваем True/False именно того pk, на который ответили правильно, ели ответили неверно, то тот который был в памяти
+            for pk in data['all_pk']:
+                await put_answer({'pk': pk, 'answer': 'False'})
--- src/bot_app/utils.py
+++ src/bot_app/utils.py
@@ -2,7 +2,20 @@ import random
 
 def get_random_word_detail(lst: list):
     random_word_detail = random.choice(lst)
-    return random_word_detail.get('translate'), random_word_detail.get('example'), random_word_detail.get('word_stat').get('pk')
+    return random_word_detail.get('example'), random_word_detail.get('word_stat').get('pk')
 
 def get_all_translate(lst: list) -> list:
     return [n.get('translate') for n in lst]
+
+def get_all_pk(lst: list) -> list:
+    return [n.get('word_stat').get('pk') for n in lst]
+
+def prepare_dict(dct: dict) -> dict:
+    word_detail_list = dct.get('words_detail')
+    random_word_detail_exaple, random_word_detail_pk = get_random_word_detail(word_detail_list)
+    return dict(word=dct.get('word'), translate_list=get_all_translate(word_detail_list), example=random_word_detail_exaple, stat_id=random_word_detail_pk, all_pk=get_all_pk(word_detail_list), all_msg=word_detail_list)
+
+def find_answer(messages: list, answer: str):
+    for message in messages:
+        if answer == message.get('translate'):
+            return message.get('word_stat').get('pk')

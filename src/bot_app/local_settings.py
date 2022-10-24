757bf03b50c5acbc74036319fcfeec4bd42d9852
Parhelia__ <ostapchukalekseyjdm@gmail.com>
2022-10-24 22:06:09 +0300

    change local settings

--- src/bot_app/local_settings.py
+++ src/bot_app/local_settings.py
@@ -1,6 +1,6 @@
 API_KEY = '5426623475:AAFrdVUpmFy6i7AqyaGPvJ0oqT5GidlX2Mk'
-WORDS_API_URL_RANDOM = 'http://127.0.0.1:8000/dictionary/persononeword/?telegram_id='
-WORDS_API_URL_ANSWER = 'http://127.0.0.1:8000/dictionary/updatepersondata/'
-WORDS_API_URL_ANSWERS = 'http://127.0.0.1:8000/dictionary/updatepersondatas/'
+WORDS_API_URL_RANDOM = 'http://89.22.239.217/dictionary/persononeword/?telegram_id='
+WORDS_API_URL_ANSWER = 'http://89.22.239.217/dictionary/updatepersondata/'
+WORDS_API_URL_ANSWERS = 'http://89.22.239.217/dictionary/updatepersondatas/'
 ####
 CNT_TRAINT_STEP = 5
